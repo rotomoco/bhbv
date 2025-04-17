f86cee28fdfb9bc68e3a28b33a39c077651479bc
rotomoco <rolandcollin44@gmail.com>
2025-04-17 18:51:30 +0200

    bilder declaration

---
+++ src/images/images.d.ts
@@ -0,0 +1,5 @@
+declare module '../images/images.js' {
+    const IMAGES: Record<string, string>;
+    export default IMAGES;
+  }
+  
--- src/images/images.js
+++ src/images/images.js
@@ -1,5 +1,6 @@
 import BHBV_Logo_2025 from './BHBV_Logo_2025.png';
 import Baerl_3592 from './Baerl_3592.jpg';
+
 const IMAGES = {
 bhbvlogo: BHBV_Logo_2025, homebg: Baerl_3592
 };
